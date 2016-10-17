cf886822656c5cf3a2129817d16bc51077353f98
Andrei Demchenko <ademchenko@by.byelex.com>
2016-10-17 17:56:13 +0300

    fix shellcheck issues

--- lab-2b/showtime.sh
+++ lab-2b/showtime.sh
@@ -11,7 +11,7 @@ function ShowtimePreCommand() {
     return
   fi
   unset showtime_at_prompt
-  showtime_command_start_time=$(eval $showtime_time_format_to_compare)
+  showtime_command_start_time=$(eval "$showtime_time_format_to_compare")
 }
 trap 'ShowtimePreCommand' DEBUG
 
@@ -26,22 +26,23 @@ function ShowtimePostCommand() {
   fi
 
   local command_start_time=$showtime_command_start_time
-  local command_end_time=$(eval $showtime_time_format_to_compare)
+  local command_end_time=""
+  command_end_time=$(eval "$showtime_time_format_to_compare")
 
   if [ "$command_start_time" -eq "$command_end_time" ]; then
     return
   fi
 
   local SEC=1
-  local MIN=$((60 * $SEC))
-  local HOUR=$((60 * $MIN))
-  local DAY=$((24 * $HOUR))
+  local MIN=$((60 * SEC))
+  local HOUR=$((60 * MIN))
+  local DAY=$((24 * HOUR))
 
-  local command_time=$(($command_end_time - $command_start_time))
-  local num_days=$(($command_time / $DAY))
-  local num_hours=$(($command_time % $DAY / $HOUR))
-  local num_mins=$(($command_time % $HOUR / $MIN))
-  local num_secs=$(($command_time % $MIN / $SEC))
+  local command_time=$((command_end_time - command_start_time))
+  local num_days=$((command_time / DAY))
+  local num_hours=$((command_time % DAY / HOUR))
+  local num_mins=$((command_time % HOUR / MIN))
+  local num_secs=$((command_time % MIN / SEC))
 
   local time_diff=""
 
@@ -58,7 +59,8 @@ function ShowtimePostCommand() {
     time_diff="${time_diff}${num_secs} sec"
   fi
 
-  local output_str="["$(ShowtimeTimeFormat $(($command_start_time)))" - "$(ShowtimeTimeFormat $(($command_end_time)))" ($time_diff)]"
+  local output_str=""
+  output_str="[$(ShowtimeTimeFormat "$command_start_time") - $(ShowtimeTimeFormat "$command_end_time") ($time_diff)]"
   local output_str_colored="\033[1;35m${output_str}\033[0m"
 
   echo -e "${output_str_colored}"

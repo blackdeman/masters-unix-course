f898ae859c1420cca3777ffd3c6373798ff6ac94
Andrew Demchenko <blackdeman@gmail.com>
2016-11-13 22:13:40 +0300

    add error checks

---
+++ lab-3/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc -std=c99 main.c -o pipeline
--- lab-3/main.c
+++ lab-3/main.c
@@ -2,10 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 void printUsage() {
-    fprintf(stderr, "Usage\n");
+    fprintf(stderr, "\nUsage: pipeline command1 | command2 | ... | commandN\n");
+}
+
+void cleanup(char** commands, int* commandStartIndexes, int** pipes, int pipesCount, pid_t* children) {
+    free(commands);
+
+    free(commandStartIndexes);
+
+    for (int i = 0; i < pipesCount; ++i) {
+        free(pipes[i]);
+    }
+    free(pipes);
+
+    free(children);
 }
 
 int main(int argc, char *argv[]) {
@@ -13,23 +27,26 @@ int main(int argc, char *argv[]) {
     // parsing arguments
 
     if (argc < 2) {
-        perror("Argument list is empty.");
+        fprintf(stderr, "Argument list is empty.");
         printUsage();
         return 2;
     }
 
-    char* commands[argc];
+    char** commands = (char**) malloc(argc * sizeof(char*));
+    if (!commands) {
+        perror("malloc");
+        return 1;
+    }
 
     int commandsCount = 0;
     int curCommandStart = -1;
 
-    int i, j;
-
-    for (i = 1; i < argc; ++i) {
+    for (int i = 1; i < argc; ++i) {
         if (strcmp(argv[i], "|") == 0) {
             if (curCommandStart < 0 || i == argc - 1) {
-                perror("Empty command found.");
+                fprintf(stderr, "Empty command found.");
                 printUsage();
+                free(commands);
                 return 2;
             } else {
                 commands[i - 1] = NULL;
@@ -47,9 +64,14 @@ int main(int argc, char *argv[]) {
     commands[argc - 1] = NULL;
 
     int commandStartIndex = 0;
-    int commandStartIndexes[commandsCount];
+    int* commandStartIndexes = (int*)malloc(commandsCount * sizeof(int));
+    if (!commandStartIndexes) {
+        perror("malloc");
+        free(commands);
+        return 1;
+    }
     commandStartIndexes[commandStartIndex++] = 0;
-    for (i = 1; i < argc; ++i) {
+    for (int i = 1; i < argc; ++i) {
         if (commands[i] == NULL) {
             commandStartIndexes[commandStartIndex++] = i + 1;
         }
@@ -57,54 +79,82 @@ int main(int argc, char *argv[]) {
 
     // running commands
 
+    pid_t* children = (pid_t*)malloc(commandsCount * sizeof(pid_t));
+    if (!children) {
+        perror("malloc");
+        free(commands);
+        free(commandStartIndexes);
+        return 1;
+    }
+
     int pipesCount = commandsCount - 1;
-    int pipes[pipesCount][2];
+    int** pipes = (int**)malloc(pipesCount * sizeof(int*));
 
-    pid_t children[commandsCount];
-
-    for (i = 0; i < pipesCount; ++i) {
+    for (int i = 0; i < pipesCount; ++i) {
+        pipes[i] = (int*)malloc(2 * sizeof(int));
+        if (!pipes[i]) {
+            perror("malloc");
+            free(commands);
+            free(commandStartIndexes);
+            free(children);
+            return 1;
+        }
         if (pipe(pipes[i]) == -1) {
-            perror("Failed to create pipe");
+            perror("pipe");
+            cleanup(commands, commandStartIndexes, pipes, pipesCount, children);
             return 1;
         }
     }
 
-    for (i = 0; i < commandsCount; ++i) {
+    for (int i = 0; i < commandsCount; ++i) {
         pid_t pid = fork();
 
         if (pid == 0) {
             char** args = commands + commandStartIndexes[i];
 
-            if (i < pipesCount && i != commandsCount - 1) {
-                close(pipes[i][0]);
-                dup2(pipes[i][1], 1);
+            if (i != commandsCount - 1) {
+                if (close(pipes[i][0]) == -1) {
+                    perror("close");
+                    cleanup(commands, commandStartIndexes, pipes, pipesCount, children);
+                }
+                if (dup2(pipes[i][1], 1) == -1) {
+                    perror("dup2");
+                    cleanup(commands, commandStartIndexes, pipes, pipesCount, children);
+                }
             }
             execvp(args[0], args);
         }
 
-        if (i < pipesCount) {
-            close(pipes[i][1]);
-            dup2(pipes[i][0], 0);
+        if (commandsCount != 1 && i < pipesCount) {
+            if (close(pipes[i][1]) == -1) {
+                perror("close");
+                cleanup(commands, commandStartIndexes, pipes, pipesCount, children);
+            }
+            if (dup2(pipes[i][0], 0) == -1) {
+                perror("pipe");
+                cleanup(commands, commandStartIndexes, pipes, pipesCount, children);
+            }
         }
 
         children[i] = pid;
     }
 
-    for (i = 0; i < commandsCount - 1; ++i)
-        for (j = 0; j < 2; ++j) {
-            close(pipes[i][j]);
-        }
-
     int exitCode = 0;
     int status;
-    for (i = 0; i < commandsCount; ++i) {
-        waitpid(children[i], &status, 0);
+    for (int i = 0; i < commandsCount; ++i) {
+        if (waitpid(children[i], &status, 0) == -1) {
+            perror("waitpid");
+            cleanup(commands, commandStartIndexes, pipes, pipesCount, children);
+            return 1;
+        }
         if (WIFEXITED(status)) {
             if (WEXITSTATUS(status) != 0) {
                 exitCode = 1;
             }
         }
     }
+
+    cleanup(commands, commandStartIndexes, pipes, pipesCount, children);
 
     return exitCode;
 }
